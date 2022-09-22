75bc45bb162635f3802779c9ef262b621dc27013
Ewelina Szmielew <ewelina@szmielew.pl>
2022-09-22 23:06:00 +0200

    Cleaning classify.py file

--- classify.py
+++ classify.py
@@ -1,26 +1,9 @@
 from preproces_data import load_data
-from tensorflow.keras.applications import inception_resnet_v2
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.applications import VGG19
-from tensorflow.keras.optimizers import SGD
-import matplotlib.pyplot as plt
-
-import os
-import re
-import glob
-import hashlib
-import argparse
-import warnings
-
-import six
-import numpy as np
-import tensorflow as tf
 from keras.models import Model
-from keras import backend as K
 from keras.layers import Dense, GlobalAveragePooling2D, Input, Flatten, BatchNormalization, Activation
-from keras.applications import InceptionResNetV2, ResNet50, ResNet152
+from keras.applications import ResNet152
 from tensorflow.keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping, ReduceLROnPlateau
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 import calendar;
 import time;
 from plot_results import plot_results
@@ -50,15 +33,15 @@ model.summary()
 
 optimizer = Adam(learning_rate=0.0001)
 model.compile(loss='categorical_crossentropy',
-              optimizer=optimizer, 
+              optimizer=optimizer,
               metrics=['accuracy'])
 
 n_epoch = 20
 
-early_stop = EarlyStopping(monitor='loss', patience=20, verbose=1, 
+early_stop = EarlyStopping(monitor='loss', patience=20, verbose=1,
                            mode='auto', restore_best_weights=True)
 
-reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, 
+reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5,
                               verbose=1, mode='auto')
 
 model.compile(
@@ -66,7 +49,7 @@ model.compile(
 )
 
 # fit model
-r = model.fit(
+result = model.fit(
     train_data,
     epochs=n_epoch,
     validation_data=val_data,
@@ -78,37 +61,24 @@ r = model.fit(
 
 # gmt stores current gmtime
 gmt = time.gmtime()
-  
+
 # ts stores timestamp
 ts = calendar.timegm(gmt)
 
-# # plot the loss
-# plt.plot(r.history["loss"], label="train loss")
-# plt.plot(r.history["val_loss"], label="val loss")
-# plt.legend()
-# plt.savefig(f"loss_{ts}")
-
-# # plot the accuracy
-# plt.plot(r.history["accuracy"], label="train acc")
-# plt.plot(r.history["val_accuracy"], label="val acc")
-# plt.legend()
-# plt.savefig(f"accuracy_{ts}")
-
-
-# # Freeze core ResNet layers and train again 
-# for layer in model.layers[-6:]:
-#    layer.trainable = False
-
+# # Freeze core ResNet layers and train again
 # # for layer in model.layers:
 # #     layer.trainable = True
 
+# for layer in model.layers[-6:]:
+#    layer.trainable = False
+
 # optimizer = Adam(learning_rate=0.0001)
 # model.compile(loss='categorical_crossentropy',
-#               optimizer=optimizer, 
+#               optimizer=optimizer,
 #               metrics=['accuracy'])
 
 # n_epoch = 25
-# history2 = model.fit_generator(generator=train_data, steps_per_epoch=STEP_SIZE_TRAIN,
+# result2 = model.fit_generator(generator=train_data, steps_per_epoch=STEP_SIZE_TRAIN,
 #                               validation_data=val_data, validation_steps=STEP_SIZE_VALID,
 #                               epochs=n_epoch,
 #                               shuffle=True,
@@ -120,11 +90,11 @@ ts = calendar.timegm(gmt)
 #                              )
 
 history = {}
-history['loss'] = r.history['loss']
-history['acc'] = range.history['accuracy']
-history['val_loss'] = r.history['val_loss']
-history['val_acc'] = r.history['val_accuracy']
-history['learning_rate'] = r.history['learning_rate']
+history['loss'] = result.history['loss']
+history['acc'] = result.history['accuracy']
+history['val_loss'] = result.history['val_loss']
+history['val_acc'] = result.history['val_accuracy']
+history['learning_rate'] = result.history['learning_rate']
 
 
 plot_results(history)
@@ -140,45 +110,3 @@ print("Prediction accuracy on validation data =", score[1])
 # Prediction accuracy on test data
 score = model.evaluate_generator(test_data)
 print("Prediction accuracy on test data =", score[1])
-# vggnet = InceptionResNetV2(
-#     input_shape=IMAGE_SIZE + [3], weights="imagenet", include_top=False
-# )
-# for layer in vggnet.layers:
-#     layer.trainable = False
-# x = Flatten()(vggnet.output)
-# prediction = Dense(train_data.num_classes, activation="softmax")(x)
-# model = Model(inputs=vggnet.input, outputs=prediction)
-# model.summary()
-
-
-# prediction = Dense(train_data.num_classes, activation="softmax")(x)
-# model.compile(
-#     loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"]
-# )
-
-# # fit model
-# r = model.fit(
-#     train_data,
-#     validation_data=val_data,
-#     epochs=10,
-#     steps_per_epoch=len(train_data),
-#     validation_steps=len(val_data),
-# )
-
-# # gmt stores current gmtime
-# gmt = time.gmtime()
-  
-# # ts stores timestamp
-# ts = calendar.timegm(gmt)
-
-# # plot the loss
-# plt.plot(r.history["loss"], label="train loss")
-# plt.plot(r.history["val_loss"], label="val loss")
-# plt.legend()
-# plt.savefig(f"loss_{ts}")
-
-# # plot the accuracy
-# plt.plot(r.history["accuracy"], label="train acc")
-# plt.plot(r.history["val_accuracy"], label="val acc")
-# plt.legend()
-# plt.savefig(f"accuracy_{ts}")
